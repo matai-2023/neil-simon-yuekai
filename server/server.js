066fef307bc7749bf37ff2993662d4625297ea70
neilsen-ngoi <112848446+neilsen-ngoi@users.noreply.github.com>
2023-08-04 09:56:32 +1200

    add server.get-:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-
 import express from 'express'
 import hbs from 'express-handlebars'
+import * as fsPromises from 'node:fs/promises'
 
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 const server = express()
 
@@ -17,14 +19,22 @@ server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 server.set('views', Path.resolve('server/views'))
 
+//get data file
+const filePath = Path.join(__dirname, 'data', 'data.json')
+console.log(filePath)
+const data = await fsPromises.readFile(filePath, 'utf-8')
+const dishes = JSON.parse(data)
+const dish = dishes.dishes
+
 // Your routes/router(s) should go here
-server.get('/', (req,res) => {
-    res.send('home')
+server.get('/', (req, res) => {
+  res.send('home')
 })
 
-server.get('/dishes/:id', (req,res) => {
+server.get('/dishes/:id', (req, res) => {
   const id = req.params.id
-  const dish = 
+  const thisDish = dish.find((obj) => obj['id'] == id)
+  res.render('dishes', thisDish)
 })
 
 export default server
